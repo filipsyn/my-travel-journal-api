13bb3fcc5c9f6dd63af6e789041319a6b922d73e
Filip S <filip.syn@protonmail.com>
2023-01-18 17:23:49 +0100

    Adjusted `Endpoints` class
    - Class is remade to assign URL to each controller individually

--- MyTravelJournal.Api/Utils/Endpoints.cs
+++ MyTravelJournal.Api/Utils/Endpoints.cs
@@ -2,13 +2,10 @@ namespace MyTravelJournal.Api.Utils;
 
 public static class Endpoints
 {
-    private const string GlobalPrefix = "/api";
-    
     public static class User
     {
-        private const string ControllerPrefix = $"{GlobalPrefix}/user";
-        
-        public const string GetUser = $"{ControllerPrefix}";
+        public const string GetAllUsers = "/api/user";
+
+        public const string GetUser = "/api/user/{id}";
     }
-    
 }
