449d3f9a0afea62ce483b1e60dca8f6e5e3ec4a8
Filip S <filip.syn@protonmail.com>
2023-01-18 17:56:43 +0100

    Added types for URL variables
    - `id` filed int the `/user` endpoints are assigned the `int` type

--- MyTravelJournal.Api/Utils/Endpoints.cs
+++ MyTravelJournal.Api/Utils/Endpoints.cs
@@ -6,12 +6,12 @@ public static class Endpoints
     {
         public const string GetAllUsers = "/api/user";
 
-        public const string GetUser = "/api/user/{id}";
+        public const string GetUser = "/api/user/{id:int}";
 
         public const string CreateUser = "/api/user/";
 
-        public const string DeleteUser = "/api/user/{id}";
+        public const string DeleteUser = "/api/user/{id:int}";
 
-        public const string UpdateUser = "/api/user/{id}";
+        public const string UpdateUser = "/api/user/{id:int}";
     }
 }
