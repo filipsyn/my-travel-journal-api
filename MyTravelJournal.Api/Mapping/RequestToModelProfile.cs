a82bb20fa78a5800805d25a954fdfbbc5161c154
Filip S <filip.syn@protonmail.com>
2023-01-26 18:47:36 +0100

    Created mappings needed for updating user info.
    - Added mappings of `JsonPatchDocuments`. Request JsonPatch is mapped onto User JsonPatch
    - Added mappings of `Operation`

--- MyTravelJournal.Api/Mapping/RequestToModelProfile.cs
+++ MyTravelJournal.Api/Mapping/RequestToModelProfile.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Operations;
 using MyTravelJournal.Api.DTOs;
 using MyTravelJournal.Api.Models;
 
@@ -10,5 +12,8 @@ public class RequestToModelProfile : Profile
     {
         CreateMap<UserDetailsDto, User>();
         CreateMap<CreateUserRequest, User>();
+        CreateMap<UpdateUserDetailsRequest, User>();
+        CreateMap<JsonPatchDocument<UpdateUserDetailsRequest>, JsonPatchDocument<User>>();
+        CreateMap<Operation<UpdateUserDetailsRequest>, Operation<User>>();
     }
 }
