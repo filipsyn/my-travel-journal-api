a6dfdb0ce66095c6b7fb8b0a3ae39b901326485d
Filip S <filip.syn@protonmail.com>
2023-01-28 12:07:15 +0100

    Created `/register` and `/login` routes on `AuthController`

--- MyTravelJournal.Api/Controllers/V1/AuthController.cs
+++ MyTravelJournal.Api/Controllers/V1/AuthController.cs
@@ -7,5 +7,15 @@ namespace MyTravelJournal.Api.Controllers.V1;
 [Route(ApiRoutes.Auth.ControllerUrl)]
 public class AuthController : ControllerBase
 {
-    
+    [HttpPost(ApiRoutes.Auth.Register)]
+    public async Task<ActionResult> Register()
+    {
+        return Ok();
+    }
+
+    [HttpPost(ApiRoutes.Auth.Login)]
+    public async Task<ActionResult> Login()
+    {
+        return Ok();
+    }
 }
