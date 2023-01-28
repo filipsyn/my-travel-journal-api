e16425d09b6a8b5fb14ad418c879e3d758d565f0
Filip S <filip.syn@protonmail.com>
2023-01-28 20:11:49 +0100

    Updated documentation of endpoints on `UsersController`

--- MyTravelJournal.Api/Controllers/V1/UsersController.cs
+++ MyTravelJournal.Api/Controllers/V1/UsersController.cs
@@ -22,10 +22,9 @@ public class UsersController : ControllerBase
     /// <summary>
     /// Retrieves list of all users.
     /// </summary>
-    /// <returns></returns>
-    /// <response code="200">All users retrieved</response>
+    /// <response code="200">All users successfully retrieved</response>
     [HttpGet(ApiRoutes.User.GetAllUsers)]
-    [ProducesResponseType(typeof(List<UserDetailsResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ServiceResponse<IEnumerable<UserDetailsResponse>>), StatusCodes.Status200OK)]
     public async Task<ActionResult<List<UserDetailsResponse>>> GetAllUsers()
     {
         var response = await _userService.GetAllAsync();
@@ -36,13 +35,12 @@ public class UsersController : ControllerBase
     /// <summary>
     /// Retrieves specific user
     /// </summary>
-    /// <param name="id">ID of searched user</param>
-    /// <returns></returns>
+    /// <param name="id">An ID of searched user</param>
     /// <response code="200">Returns information about specific user</response>
     /// <response code="404">User with this ID was not found</response>
     [HttpGet(ApiRoutes.User.GetUserById)]
-    [ProducesResponseType(typeof(UserDetailsResponse), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ServiceResponse<UserDetailsResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ServiceResponse<>), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<UserDetailsResponse>> GetUserById(int id)
     {
         var response = await _userService.GetByIdAsync(id);
@@ -54,12 +52,11 @@ public class UsersController : ControllerBase
     /// Creates new user
     /// </summary>
     /// <param name="request"></param>
-    /// <returns></returns>
     /// <response code="200">New user was successfully added</response>
     /// <response code="409">Raised error when sending data to database</response>
     [HttpPost(ApiRoutes.User.CreateUser)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [ProducesResponseType(typeof(ServiceResponse<>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ServiceResponse<>), StatusCodes.Status409Conflict)]
     public async Task<ActionResult> CreateUser([FromBody] CreateUserRequest request)
     {
         // TODO: Document response code 204
@@ -73,14 +70,13 @@ public class UsersController : ControllerBase
     /// Deletes specific user
     /// </summary>
     /// <param name="id">ID of user to be deleted</param>
-    /// <returns></returns>
     /// <response code="200">Successfully deleted user</response>
     /// <response code="404">User with this ID wasn't found</response>
     /// <response code="409">Raised error when sending data to database</response>
     [HttpDelete(ApiRoutes.User.DeleteUser)]
-    [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [ProducesResponseType(typeof(ServiceResponse<>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ServiceResponse<>), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ServiceResponse<>), StatusCodes.Status409Conflict)]
     public async Task<ActionResult<int>> DeleteUser(int id)
     {
         var response = await _userService.DeleteByIdAsync(id);
@@ -89,8 +85,8 @@ public class UsersController : ControllerBase
     }
 
     /// <summary>Performs partial update of specific user's data.</summary>
-    /// <param name="patch">Request body</param>
-    /// <param name="id">ID of updated user</param>
+    /// <param name="patch">A request body</param>
+    /// <param name="id">An ID of updated user</param>
     /// <remarks>
     ///     Request has a structure of standard JSON patch request.
     /// 
@@ -106,18 +102,18 @@ public class UsersController : ControllerBase
     ///     ]
     ///     ```
     /// </remarks>
-    /// <response code="204">Successfully updated</response>
+    /// <response code="200">Successfully updated</response>
     /// <response code="400">Invalid model state</response>
     /// <response code="404">Updating not existing user</response>
     /// <response code="409">Raised error when sending data to database</response>
     /// <response code="500">Mistake when mapping User to DTO</response>
     /// 
     [HttpPatch(ApiRoutes.User.UpdateUser)]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(ServiceResponse<>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ServiceResponse<>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ServiceResponse<>), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ServiceResponse<>), StatusCodes.Status409Conflict)]
+    [ProducesResponseType(typeof(ServiceResponse<>), StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult<string>> UpdateUser([FromBody] JsonPatchDocument<UpdateUserDetailsRequest> patch,
         int id)
     {
