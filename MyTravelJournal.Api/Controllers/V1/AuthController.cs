e6863c4bbc1e4300ab5a5e515dbd41ab48d0436a
Filip S <filip.syn@protonmail.com>
2023-01-28 12:03:34 +0100

    Set base route for `AuthController`

--- MyTravelJournal.Api/Controllers/V1/AuthController.cs
+++ MyTravelJournal.Api/Controllers/V1/AuthController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using MyTravelJournal.Api.Contracts.V1;
 
 namespace MyTravelJournal.Api.Controllers.V1;
 
 [ApiController]
+[Route(ApiRoutes.Auth.ControllerUrl)]
 public class AuthController : ControllerBase
 {
     
