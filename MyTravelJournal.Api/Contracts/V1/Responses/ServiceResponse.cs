c3070e944a9312f3f38a873b4a82f32fa9814823
Filip S <filip.syn@protonmail.com>
2023-01-28 16:58:48 +0100

    Added basic documentation to `ServiceResponse` contract

--- MyTravelJournal.Api/Contracts/V1/Responses/ServiceResponse.cs
+++ MyTravelJournal.Api/Contracts/V1/Responses/ServiceResponse.cs
@@ -1,5 +1,9 @@
 namespace MyTravelJournal.Api.Contracts.V1.Responses;
 
+/// <summary>
+/// Contract defining response of services
+/// </summary>
+/// <typeparam name="T">Type of data returned by service</typeparam>
 public class ServiceResponse<T>
 {
     public T? Data { get; init; } = default(T);
