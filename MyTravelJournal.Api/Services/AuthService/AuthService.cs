4d4219a053f9080160ca75e50981a06b69f10933
Filip S <filip.syn@protonmail.com>
2023-01-29 14:00:32 +0100

    Implemented `RegisterAsync` method in `AuthService`

--- MyTravelJournal.Api/Services/AuthService/AuthService.cs
+++ MyTravelJournal.Api/Services/AuthService/AuthService.cs
@@ -1,6 +1,40 @@
+using AutoMapper;
+using MyTravelJournal.Api.Contracts.V1.Requests;
+using MyTravelJournal.Api.Contracts.V1.Responses;
+using MyTravelJournal.Api.Models;
+using MyTravelJournal.Api.Services.UserService;
+
 namespace MyTravelJournal.Api.Services.AuthService;
 
 public class AuthService : IAuthService
 {
-    
+    private readonly IUserService _userService;
+    private readonly IMapper _mapper;
+
+    public AuthService(IUserService userService, IMapper mapper)
+    {
+        _userService = userService;
+        _mapper = mapper;
+    }
+    public async Task<ServiceResponse<string>> RegisterAsync(CreateUserRequest request)
+    {
+        var foundUser = await _userService.GetByUsernameAsync(request.Username);
+        if (foundUser.Success)
+        {
+            return new ServiceResponse<string>
+            {
+                Success = false,
+                Details = new StatusDetails
+                {
+                    Code = StatusCodes.Status400BadRequest,
+                    Message = "User with this username already exists."
+                }
+            };
+        }
+
+        var user = _mapper.Map<User>(request);
+
+        return await _userService.CreateAsync(request);
+    }
+
 }
--- MyTravelJournal.Api/Services/AuthService/IAuthService.cs
+++ MyTravelJournal.Api/Services/AuthService/IAuthService.cs
@@ -1,6 +1,18 @@
+using MyTravelJournal.Api.Contracts.V1.Requests;
+using MyTravelJournal.Api.Contracts.V1.Responses;
+
 namespace MyTravelJournal.Api.Services.AuthService;
 
 public interface IAuthService
 {
-    
+    // Register Async
+    public Task<ServiceResponse<string>> RegisterAsync(CreateUserRequest request);
+
+    // Login Async
+
+    // Generate Token
+
+    // Authenticate Password
+
+    // Generate Refresh Token
 }
