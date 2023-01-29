ea98aee1a4a4e9233dddf30f2649513a4044528c
Filip S <filip.syn@protonmail.com>
2023-01-29 14:00:57 +0100

    Reordering of properties and added password in `CreateUserRequest`

--- MyTravelJournal.Api/Contracts/V1/Requests/CreateUserRequest.cs
+++ MyTravelJournal.Api/Contracts/V1/Requests/CreateUserRequest.cs
@@ -2,8 +2,9 @@ namespace MyTravelJournal.Api.Contracts.V1.Requests;
 
 public record CreateUserRequest
 {
+    public string Username { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
     public string FirstName { get; init; } = string.Empty;
     public string LastName { get; init; } = string.Empty;
-    public string Username { get; init; } = string.Empty;
     public string Email { get; init; } = string.Empty;
 }
