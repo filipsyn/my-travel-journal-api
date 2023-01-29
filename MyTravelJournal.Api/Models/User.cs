a35611eade0294746790e35003d84cd8f6099064
Filip S <filip.syn@protonmail.com>
2023-01-29 13:02:25 +0100

    Removed password salt attribute from users table in database

--- MyTravelJournal.Api/Models/User.cs
+++ MyTravelJournal.Api/Models/User.cs
@@ -8,5 +8,4 @@ public class User
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public byte[] PasswordHash { get; set; } = new byte[32];
-    public byte[] PasswordSalt { get; set; } = new byte[32];
 }
