6f72470047970fe6161be5cb2d196167e7b3c521
Filip S <filip.syn@protonmail.com>
2023-02-02 16:10:38 +0100

    Created `TripDetailsResponse` contract.

---
+++ MyTravelJournal.Api/Contracts/V1/Responses/TripDetailsResponse.cs
@@ -0,0 +1,13 @@
+namespace MyTravelJournal.Api.Contracts.V1.Responses;
+
+public record TripDetailsResponse
+{
+    public int TripId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public DateTime Start { get; set; } = DateTime.Now;
+    public DateTime End { get; set; } = DateTime.Now;
+
+    public int UserId { get; set; }
+}
