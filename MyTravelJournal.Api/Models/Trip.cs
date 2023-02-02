4b45ad244dbf682be055ce14ce525de6008eac37
Filip S <filip.syn@protonmail.com>
2023-02-02 15:11:47 +0100

    Created `Trip` model.
    - Created class `Trip` which will represent `trips` table in the database.

---
+++ MyTravelJournal.Api/Models/Trip.cs
@@ -0,0 +1,11 @@
+namespace MyTravelJournal.Api.Models;
+
+public class Trip
+{
+    public int TripId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public DateTime Start { get; set; } = DateTime.Now;
+    public DateTime End { get; set; } = DateTime.Now;
+}
