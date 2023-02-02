0b3832918bd6f4d16b942ecee748c9471da52fdd
Filip S <filip.syn@protonmail.com>
2023-02-02 16:01:11 +0100

    Assigned base route to trips controller.
    - Also declared that all endpoints produce json responses.

--- MyTravelJournal.Api/Controllers/V1/TripsController.cs
+++ MyTravelJournal.Api/Controllers/V1/TripsController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using MyTravelJournal.Api.Contracts.V1;
 
 namespace MyTravelJournal.Api.Controllers.V1;
 
 [ApiController]
+[Route(ApiRoutes.Trip.ControllerUrl)]
+[Produces("application/json")]
 public class TripsController : ControllerBase
 {
-    
 }
