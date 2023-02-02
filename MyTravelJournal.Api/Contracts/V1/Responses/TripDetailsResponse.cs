dcdf85e0402a324f17d9cc5e2ac9afe29397fa8d
Filip S <filip.syn@protonmail.com>
2023-02-02 16:27:05 +0100

    Changed order of properties in `TripDetailsResponse`.
    - `UserId` moved to to be second.

--- MyTravelJournal.Api/Contracts/V1/Responses/TripDetailsResponse.cs
+++ MyTravelJournal.Api/Contracts/V1/Responses/TripDetailsResponse.cs
@@ -3,11 +3,11 @@ namespace MyTravelJournal.Api.Contracts.V1.Responses;
 public record TripDetailsResponse
 {
     public int TripId { get; set; }
+    public int UserId { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Location { get; set; } = string.Empty;
     public DateTime Start { get; set; } = DateTime.Now;
     public DateTime End { get; set; } = DateTime.Now;
 
-    public int UserId { get; set; }
 }
