d297d4eb1a07042975e3fabb104890d821f4400e
Filip S <filip.syn@protonmail.com>
2023-02-02 17:14:19 +0100

    Added constructors to `ServiceResponse` class.
    - Created 2 constructors. In both constructors are specified: status code, message, data (nullable), and in the one constructor can be passed success status.

--- MyTravelJournal.Api/Contracts/V1/Responses/ServiceResponse.cs
+++ MyTravelJournal.Api/Contracts/V1/Responses/ServiceResponse.cs
@@ -1,3 +1,4 @@
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 
 namespace MyTravelJournal.Api.Contracts.V1.Responses;
@@ -13,4 +14,29 @@ public class ServiceResponse<T>
 
     [JsonIgnore] public bool Success { get; init; }
     public StatusDetails Details { get; init; } = new();
+
+
+    public ServiceResponse(int code, string message, bool success, T? data = default(T))
+    {
+        Details.Code = code;
+        Details.Message = !message.IsNullOrEmpty() ? message : string.Empty;
+        Success = success;
+        Data = data;
+    }
+
+    public ServiceResponse(int code, string message, T? data = default(T))
+    {
+        Details.Code = code;
+        Details.Message = !message.IsNullOrEmpty() ? message : string.Empty;
+
+        // In this variant of constructor the status is automatically decided from the status code.
+        Success = code is >= 100 and < 400;
+
+        Data = data;
+    }
+
+
+    public ServiceResponse()
+    {
+    }
 }
