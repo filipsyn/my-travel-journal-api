1c3fa4cadaddd0b84a60d4c8315e67b188e2646d
Filip S <filip.syn@protonmail.com>
2023-02-04 14:51:33 +0100

    Update endpoint route turned public.

--- MyTravelJournal.Api/Contracts/V1/ApiRoutes.cs
+++ MyTravelJournal.Api/Contracts/V1/ApiRoutes.cs
@@ -39,7 +39,7 @@ public static class ApiRoutes
         public const string GetById = "{id:int}";
 
         public const string Create = "";
-        
-        private const string Update = "{id:int}";
+
+        public const string Update = "{id:int}";
     }
 }
