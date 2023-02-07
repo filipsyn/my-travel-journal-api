df7369945f0ab8a5f0c3311c06e835d321633349
Filip S <filip.syn@protonmail.com>
2023-02-07 18:29:20 +0100

    Implemented `UpdateAsync` method in trips service.

--- MyTravelJournal.Api/Services/TripService/TripService.cs
+++ MyTravelJournal.Api/Services/TripService/TripService.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.EntityFrameworkCore;
 using MyTravelJournal.Api.Contracts.V1.Requests;
 using MyTravelJournal.Api.Contracts.V1.Responses;
@@ -69,6 +70,45 @@ public class TripService : ITripService
         return new ServiceResponse<TripDetailsResponse>(
             StatusCodes.Status200OK,
             "Trip was successfully added."
+        );
+    }
+
+    public async Task<ServiceResponse<TripDetailsResponse>> UpdateAsync(JsonPatchDocument<UpdateTripRequest> request,
+        int id)
+    {
+        var trip = await _db.Trips.FirstOrDefaultAsync(t => t.TripId == id);
+        if (trip is null)
+        {
+            return new ServiceResponse<TripDetailsResponse>(StatusCodes.Status404NotFound,
+                "Trip with this ID was not found.");
+        }
+
+        var patchedTrip = _mapper.Map<JsonPatchDocument<Trip>>(request);
+        if (patchedTrip is null)
+        {
+            return new ServiceResponse<TripDetailsResponse>(
+                StatusCodes.Status500InternalServerError,
+                "Mapping of objects was unsuccessfully."
+            );
+        }
+
+        patchedTrip.ApplyTo(trip);
+
+        try
+        {
+            await _db.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            return new ServiceResponse<TripDetailsResponse>(
+                StatusCodes.Status409Conflict,
+                ex.ToString()
+            );
+        }
+
+        return new ServiceResponse<TripDetailsResponse>(
+            StatusCodes.Status200OK,
+            "Trip was successfully updated."
         );
     }
 }
