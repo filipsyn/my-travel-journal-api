920ad8bef5920850b1fc559fbd462c66028bf58c
Filip S <filip.syn@protonmail.com>
2023-02-07 18:06:16 +0100

    Implemented GetAllAsync method on trips service.
    Added signature to interface and added implementation of of GetAllAsync method. Also was injected DataContext and Mapper instances into trip service.

--- MyTravelJournal.Api/Services/TripService/ITripService.cs
+++ MyTravelJournal.Api/Services/TripService/ITripService.cs
@@ -1,6 +1,8 @@
+using MyTravelJournal.Api.Contracts.V1.Responses;
+
 namespace MyTravelJournal.Api.Services.TripService;
 
 public interface ITripService
 {
-    
+   public Task<ServiceResponse<IEnumerable<TripDetailsResponse>>> GetAllAsync();
 }
--- MyTravelJournal.Api/Services/TripService/TripService.cs
+++ MyTravelJournal.Api/Services/TripService/TripService.cs
@@ -1,5 +1,28 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MyTravelJournal.Api.Contracts.V1.Responses;
+using MyTravelJournal.Api.Data;
+
 namespace MyTravelJournal.Api.Services.TripService;
 
 public class TripService : ITripService
 {
+    private readonly DataContext _db;
+    private readonly IMapper _mapper;
+
+    public TripService(DataContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
+    public async Task<ServiceResponse<IEnumerable<TripDetailsResponse>>> GetAllAsync()
+    {
+        var trips = await _db.Trips.ToListAsync();
+
+        return new ServiceResponse<IEnumerable<TripDetailsResponse>>(
+            StatusCodes.Status200OK,
+            "List of trips successfully retrieved.",
+            _mapper.Map<IEnumerable<TripDetailsResponse>>(trips)
+        );
+    }
 }
