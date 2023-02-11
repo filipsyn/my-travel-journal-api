7224b4f63c76541949b42eb69e4ea37d89d857ed
Filip S <filip.syn@protonmail.com>
2023-02-11 15:30:26 +0100

    Added `Role` property to `UserDetailsResponse` contract.

--- MyTravelJournal.Api/Contracts/V1/Responses/UserDetailsResponse.cs
+++ MyTravelJournal.Api/Contracts/V1/Responses/UserDetailsResponse.cs
@@ -7,4 +7,5 @@ public record UserDetailsResponse
     public string FirstName { get; init; } = string.Empty;
     public string LastName { get; init; } = string.Empty;
     public string Email { get; init; } = string.Empty;
+    public string Role { get; init; } = string.Empty;
 };
