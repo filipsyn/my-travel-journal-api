3317f3c78023ee59f20fbc830986cd6c7736cbca
Filip S <filip.syn@protonmail.com>
2023-02-11 15:30:02 +0100

    Added `Role` property to `User` model.
    First part of finishing role-based authorization. Added role property to User model. Applied migration to database.

--- MyTravelJournal.Api/Models/User.cs
+++ MyTravelJournal.Api/Models/User.cs
@@ -7,6 +7,7 @@ public class User
     public string LastName { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+    public string Role { get; set; } = "User";
     public string PasswordHash { get; set; } = string.Empty;
 
     public List<Trip>? Trips { get; set; }
