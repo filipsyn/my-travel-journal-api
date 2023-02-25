de792d9a3c188e5f1ac08d4f4ab0dd585e1c9fc5
Filip S <filip.syn@protonmail.com>
2023-02-25 14:26:51 +0100

    Added unit test setup and first test of `UserService`.

--- MyTravelJournal.Tests/UserServiceTests.cs
+++ MyTravelJournal.Tests/UserServiceTests.cs
@@ -1,6 +1,70 @@
+using System.Threading.Tasks;
+using AutoMapper;
+using Moq;
+using MyTravelJournal.Api.Contracts.V1.Responses;
+using MyTravelJournal.Api.Models;
+using MyTravelJournal.Api.Repositories.UserRepository;
+using MyTravelJournal.Api.Services.TripService;
+using MyTravelJournal.Api.Services.UserService;
+using Xunit;
 
 namespace MyTravelJournal.Tests;
 
 public class UserServiceTests
 {
+    private readonly UserService _sut;
+    private readonly Mock<IUserRepository> _userRepoMock = new();
+    private readonly Mock<ITripService> _tripServiceMock = new();
+
+    public UserServiceTests()
+    {
+        var config = new MapperConfiguration(cfg =>
+            cfg.CreateMap<User, UserDetailsResponse>()
+        );
+
+        var mapper = config.CreateMapper();
+
+        _sut = new UserService(
+            mapper: mapper,
+            tripService: _tripServiceMock.Object,
+            userRepository: _userRepoMock.Object
+        );
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnUser_WhenUserExists()
+    {
+        // Arrange
+        var userId = 1;
+        var userFirstName = "Tom";
+        var userLastName = "Smith";
+        var userEmail = "tom.smith@example.com";
+        var userRole = "User";
+        var userUsername = "tommy.smith";
+
+        var expected = new User
+        {
+            UserId = userId,
+            FirstName = userFirstName,
+            LastName = userLastName,
+            Email = userEmail,
+            Role = userRole,
+            Username = userUsername
+        };
+
+        _userRepoMock
+            .Setup(x => x.GetByIdAsync(userId))
+            .ReturnsAsync(expected);
+
+        // Act
+        var actual = await _sut.GetByIdAsync(userId);
+
+        // Assert
+        Assert.Equal(expected.UserId, actual.UserId);
+        Assert.Equal(expected.FirstName, actual.FirstName);
+        Assert.Equal(expected.LastName, actual.LastName);
+        Assert.Equal(expected.Email, actual.Email);
+        Assert.Equal(expected.Role, actual.Role);
+        Assert.Equal(expected.Username, actual.Username);
+    }
 }
